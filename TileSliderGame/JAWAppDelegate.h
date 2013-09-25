daae18aae7cfa005d108b14cfd70fa4365f3a509
Josh Walker <josh@lwmn00x194199-172027173024.amer.carlson.com>
2013-09-25 08:22:15 -0500

    Initial Commit

---
+++ TileSliderGame/JAWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JAWAppDelegate.h
+//  TileSliderGame
+//
+//  Created by Joshua Walker on 9/25/13.
+//  Copyright (c) 2013 joshwalker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JAWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TileSliderGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TileSliderGame
+//
+//  Created by Joshua Walker on 9/25/13.
+//  Copyright (c) 2013 joshwalker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JAWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JAWAppDelegate class]));
+    }
+}
